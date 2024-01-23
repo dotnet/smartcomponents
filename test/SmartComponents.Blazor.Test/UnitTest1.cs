6ce264a091a49c03cfda4dfe82245f49c6828f02
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2024-01-23 14:59:22 +0000

    Add Playwright

--- test/SmartComponents.Blazor.Test/UnitTest1.cs
+++ test/SmartComponents.Blazor.Test/UnitTest1.cs
@@ -1,10 +1,19 @@
+using Microsoft.Playwright;
+using static Microsoft.Playwright.Assertions;
+
 namespace SmartComponents.Blazor.Test;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public async Task Test1()
     {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync();
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("https://playwright.dev/dotnet");
 
+
+        await Expect(page.GetByRole(AriaRole.Link, new() { Name = "Get started" })).ToBeVisibleAsync();
     }
 }
