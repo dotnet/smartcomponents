8df684d48fe7425dcf18694931ad6e59c8847948
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2024-01-24 15:16:26 +0000

    Include JS in MVC apps via another tag helper

---
+++ src/SmartComponents.Mvc/SmartComponentsScript.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SmartComponents.Mvc;
+
+[HtmlTargetElement("smart-components-script", TagStructure = TagStructure.NormalOrSelfClosing)]
+public class SmartComponentsScript : TagHelper
+{
+    [ViewContext]
+    public ViewContext ViewContext { get; set; } = default!;
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        var httpContext = ViewContext.HttpContext;
+        var fileVersionProvider = httpContext.RequestServices.GetRequiredService<IFileVersionProvider>();
+        var pathBase = httpContext.Request.PathBase;
+        var relativeSrc = UriHelper.BuildRelative(
+            pathBase: pathBase,
+            "/_content/SmartComponents.WebUI/SmartComponents.WebUI.lib.module.js");
+        var srcWithFileVersion = fileVersionProvider.AddFileVersionToPath(pathBase, relativeSrc);
+
+        output.TagName = "script";
+        output.TagMode = TagMode.StartTagAndEndTag;
+        output.Attributes.Add("src", srcWithFileVersion);
+        output.Content.Clear();
+    }
+}
--- src/SmartComponents.Mvc/TestComponent.cs
+++ src/SmartComponents.Mvc/TestComponent.cs
@@ -1,19 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
-using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using System.Text.Encodings.Web;
 
-namespace SmartComponents.Mvc
+namespace SmartComponents.Mvc;
+
+[HtmlTargetElement("test-component", TagStructure = TagStructure.NormalOrSelfClosing)]
+public class TestComponent : TagHelper
 {
-    [HtmlTargetElement("test-component", TagStructure = TagStructure.NormalOrSelfClosing)]
-    public class TestComponent : TagHelper
+    public override void Process(TagHelperContext context, TagHelperOutput output)
     {
-        public override void Process(TagHelperContext context, TagHelperOutput output)
-        {
-            output.TagName = "div";
-            output.TagMode = TagMode.StartTagAndEndTag;
-            output.AddClass("my-component", HtmlEncoder.Default);
-            output.Content.Append("This is a test component. TODO: Real ones.");
-        }
+        output.TagName = "div";
+        output.TagMode = TagMode.StartTagAndEndTag;
+        output.AddClass("my-component", HtmlEncoder.Default);
+        output.Content.Append("This is a test component. TODO: Real ones.");
     }
 }
