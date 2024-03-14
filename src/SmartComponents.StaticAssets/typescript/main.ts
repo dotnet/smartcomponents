dca03d7190d48f7e715b5aa9f0f57c4c66b9a1bb
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2024-03-14 15:26:56 +0000

    Only load the JS once (and do so as a module to avoid clashing top-level funcs/vars)

--- src/SmartComponents.StaticAssets/typescript/main.ts
+++ src/SmartComponents.StaticAssets/typescript/main.ts
@@ -1,7 +1,13 @@
-﻿import { registerSmartComboBoxCustomElement } from './SmartComboBox';
+import { registerSmartComboBoxCustomElement } from './SmartComboBox';
 import { registerSmartPasteClickHandler } from './SmartPaste';
 import { registerSmartTextAreaCustomElement } from './SmartTextArea/SmartTextArea';
 
-registerSmartComboBoxCustomElement();
-registerSmartPasteClickHandler();
-registerSmartTextAreaCustomElement();
+// Only run this script once. If you import it multiple times, the 2nd-and-later are no-ops.
+const isLoadedMarker = '__smart_components_loaded__';
+if (!Object.getOwnPropertyDescriptor(document, isLoadedMarker)) {
+    Object.defineProperty(document, isLoadedMarker, { enumerable: false, writable: false });
+
+    registerSmartComboBoxCustomElement();
+    registerSmartPasteClickHandler();
+    registerSmartTextAreaCustomElement();
+}
